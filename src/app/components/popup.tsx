86c305c926583db20f4868ff17fa8e52f393e587
sabiyou <luciusemuh@gmail.com>
2025-05-27 13:08:02 +0100

    Deployment Corrections

--- src/app/components/popup.tsx
+++ src/app/components/popup.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import CheckLottie from "./checkLottie";
 
 type ModalProps = {
   isOpen: boolean;
